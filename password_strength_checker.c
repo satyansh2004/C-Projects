0f21d5479a9075d152444765e6e5a326013802f0
Satyansh Chand <satyanshchand01@gmail.com>
2026-06-01 00:45:29 +0530

    Completed Password Strength Checker Algo

--- password_strength_checker.c
+++ password_strength_checker.c
@@ -11,7 +11,7 @@ int checkLength(char password[])
     return i - 1;
 }
 
-bool containDigit(char password[], int nums[])
+bool containInt(char password[], int nums[])
 {
     int length = checkLength(password);
 
@@ -53,6 +53,49 @@ end:
     return false;
 }
 
+bool containsLowerCase(char password[])
+{
+    int length = checkLength(password);
+    for (int i = 0; i < length; i++)
+    {
+        char ch = password[i];
+        int asciiValue = (int)ch;
+        for (int j = 97; j <= 122; j++)
+        {
+            if (asciiValue == j)
+            {
+                return true;
+                goto end;
+            }
+        }
+    }
+end:
+
+    return false;
+}
+
+bool hasSymbols(char password[])
+{
+    int length = checkLength(password);
+    for (int i = 0; i < length; i++)
+    {
+        char ch = password[i];
+        int asciiValue = (int)ch;
+        for (int j = 35; j <= 38; j++)
+        {
+            if (asciiValue == j || asciiValue == 64)
+            {
+                return true;
+                goto end;
+            }
+        }
+    }
+end:
+
+    return false;
+}
+
+int scoringPassword(int, int, int, int, int);
 int main(void)
 {
     char password[20];
@@ -61,7 +104,42 @@ int main(void)
     printf("Enter password: ");
     fgets(password, 20, stdin);
 
-    int length = checkLength(password);                   // Return Length
-    int digitContainCheck = containDigit(password, nums); // Return true (1) or false (0)
-    int hasUpperCase = containsUpperCase(password);       // Return true (1) or false (0)
+    int length = checkLength(password);                 // Return Length
+    int digitContainCheck = containInt(password, nums); // Return true (1) or false (0)
+    int hasUpperCase = containsUpperCase(password);     // Return true (1) or false (0)
+    int hasLowerCase = containsLowerCase(password);     // Return true (1) or false (0)
+    int doesHasSymbols = hasSymbols(password);          // Return true (1) or false (0)
+
+    int passwordScore = scoringPassword(length, digitContainCheck, hasUpperCase, hasLowerCase, doesHasSymbols);
+
+    if (passwordScore < 10)
+    {
+        printf("\nPassword Strength: Weak");
+    }
+    if (passwordScore > 10 && passwordScore <= 20)
+    {
+        printf("\nPassword Strength: Medium");
+    }
+    if (passwordScore > 20)
+    {
+        printf("\nPassword Strength: Strong");
+    }
+}
+
+int scoringPassword(int length, int digitContainCheck, int hasUpperCase, int hasLowerCase, int doesHasSymbols)
+{
+    int score = 0;
+
+    if (length >= 8)
+        score = score + 5;
+    if (digitContainCheck == 1)
+        score = score + 5;
+    if (hasUpperCase)
+        score = score + 5;
+    if (hasLowerCase)
+        score = score + 5;
+    if (doesHasSymbols)
+        score = score + 5;
+
+    return score;
 }
