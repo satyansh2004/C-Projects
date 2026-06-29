59c26c023f893ccb0e073fcdcd3c9e8f49e6d434
Satyansh Chand <satyanshchand01@gmail.com>
2026-06-30 00:38:22 +0530

    Half way done

--- student_database.c
+++ student_database.c
@@ -2,12 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Student
+typedef struct Student
 {
     int roll;
     char name[50];
     float marks;
 } std;
+
+float average_marks(std *student, int number_students)
+{
+    float sum = 0;
+    for (int i = 0; i < number_students; i++)
+    {
+        sum = sum + student->marks;
+    }
+
+    int average = sum / number_students;
+
+    return average;
+}
+
+void print_topper(std *student, int number_students)
+{
+    for (int i = 0; i < number_students; i++)
+    {
+        float max = student[i].marks;
+        for (int j = i; j < number_students + 1; j++)
+        {
+            if (student[i].marks > max)
+            {
+                max = student[i].marks;
+                printf("Max num: %f\n", max);
+            }
+            else
+            {
+                printf("Else Max num: %f\n", max);
+            }
+        }
+    }
+}
 
 int main(void)
 {
@@ -41,13 +74,17 @@ int main(void)
     }
 
     printf("\n");
-    for (int i = 0; i < number_students; i++)
-    {
-        printf("\nName: %s\nRoll: %d\nMarks: %.2f", (*std_ptr).name, (*std_ptr).roll, (*std_ptr).marks);
-        printf("\n");
-        std_ptr++;
-    }
-    free(std_ptr);
+    // for (int i = 0; i < number_students; i++)
+    // {
+    //     printf("\nName: %s\nRoll: %d\nMarks: %.2f", (*std_ptr).name, (*std_ptr).roll, (*std_ptr).marks);
+    //     printf("\n");
+    //     std_ptr++;
+    // }
+    // float a = average_marks(std_ptr, number_students);
+    // printf("Average Marks: %.2f", a);
+    print_topper(std_ptr, number_students);
+
+    // free(std_ptr);
 
     return 0;
 }
